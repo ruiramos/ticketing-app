a8a5f60b9ed8b1833fc975e3511b6be86099f0f8
Rui Ramos <ruiramos@gmail.com>
2025-05-26 23:08:04 +0100

    sign in and out

--- src/components/AdminLayout.tsx
+++ src/components/AdminLayout.tsx
@@ -1,13 +1,22 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import type { ReactNode } from 'react';
+import { useEffect, type ReactNode } from 'react';
 import { trpc, type RouterOutput } from '~/utils/trpc';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { Button } from './ui/button';
 
 type AdminLayoutProps = { children: ReactNode };
 type GetUserOutput = RouterOutput['user']['getUser'];
 
 export const AdminLayout = ({ children }: AdminLayoutProps) => {
   const { data: user } = trpc.user.getUser.useQuery();
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      void signIn();
+    }
+  }, [status]);
 
   return (
     <>
@@ -26,9 +35,9 @@ export const AdminLayout = ({ children }: AdminLayoutProps) => {
 
 const AdminHeader = ({ user }: { user: GetUserOutput }) => {
   return (
-    <div className="bg-white flex justify-between py-3 px-4 text-sm border-b border-gray-200">
+    <div className="bg-white flex justify-between py-2 px-4 text-sm border-b border-gray-200 items-center">
       <div className="w-1/4">Ticketing Admin</div>
-      <div className="flex gap-2 font-medium text-gray-400">
+      <div className="flex gap-2 font-medium text-gray-400 items-center">
         <Link href="/admin" className="no-underline text-primary">
           Events
         </Link>
@@ -38,6 +47,14 @@ const AdminHeader = ({ user }: { user: GetUserOutput }) => {
       </div>
       <div className="w-1/4 text-right text-muted-foreground">
         Hello {user?.name}!
+        <Button
+          variant="outline"
+          size="sm"
+          className="ml-2"
+          onClick={() => void signOut({ callbackUrl: '/' })}
+        >
+          Sign Out
+        </Button>
       </div>
     </div>
   );
