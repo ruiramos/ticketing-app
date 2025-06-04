4943d6397f3de6bec7a17adc5e111416ca1a2c14
Rui Ramos <ruiramos@gmail.com>
2025-06-04 10:30:27 +0100

    styling

--- src/components/DefaultLayout.tsx
+++ src/components/DefaultLayout.tsx
@@ -1,9 +1,15 @@
 import Head from 'next/head';
 import type { ReactNode } from 'react';
+import { useRouter } from 'next/router';
+import { cn } from '~/lib/utils';
 
 type DefaultLayoutProps = { children: ReactNode };
 
 export const DefaultLayout = ({ children }: DefaultLayoutProps) => {
+  const router = useRouter();
+  // embed can be "true", "false", true, false, or undefined in query
+  const embed = router.query.embed === 'true' || 'embed' in router.query;
+
   return (
     <>
       <Head>
@@ -11,8 +17,18 @@ export const DefaultLayout = ({ children }: DefaultLayoutProps) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="min-h-screen p-3 lg:p-6 bg-gray-100">
-        <div className="bg-white p-6 lg:p-8 lg:max-w-screen-xl lg:mx-auto rounded shadow-sm">
+      <main
+        className={cn(
+          'min-h-screen lg:p-6 ',
+          embed ? 'md:p-3 md:bg-gray-100' : 'p-2 bg-gray-100',
+        )}
+      >
+        <div
+          className={cn(
+            'bg-white lg:p-8 lg:max-w-screen-xl lg:mx-auto',
+            embed ? 'md:p-4 md:rounded md:shadow-sm' : 'p-4 rounded shadow-sm',
+          )}
+        >
           {children}
         </div>
       </main>
